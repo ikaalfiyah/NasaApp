15614f4b296b55e53fe1099209250c805e764965
ikaalfiyah <81912976+ikaalfiyah@users.noreply.github.com>
2021-04-04 20:29:34 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=HrGJWwukvvwbzvobGCDqB5mt5MmncsgTgZqIUjQZ";
         
         $("#loadImage").click(function(){
             $.ajax({
